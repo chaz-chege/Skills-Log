d772bec04ee9b472d3f82b2fbaa40427b3441517
chaz-chege <chazcxtine@gmail.com>
2026-09-07 15:14:57 +0300

    Task: Changed for i in range (N) to N//2 to maximize efficiency ensuring the algorithm doesn't loop through the entire word twice but instead meet in the middle

--- palindrome_check.py
+++ palindrome_check.py
@@ -1,8 +1,8 @@
 #Problem:Write a function that takes a string and returns True if it reads the same forwards and backwards, False otherwise.
-word = "racecar"
+word = input("Enter a word you would like to check if it is a palindrome:")
 def is_palindrome(word):
     N = len(word)
-    for i in range(N):
+    for i in range(N//2):
         if word[i] != word[N-1-i]:
             return False
     return True    
